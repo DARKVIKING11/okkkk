14d9b9c96fcb542bd4e7532a0a4602032ca804bd
DARKVIKING11 <72546090+DARKVIKING11@users.noreply.github.com>
2024-01-28 13:40:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Boys in Hiskid Zone","Listening to One love ","Playing Hiskids New Video"];
 
 
 let currentIndex = 0;
